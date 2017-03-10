4a124261ecf8f8dc0545562ac2bf9e352c797fd3
James <jwin4740@gmail.com>
2017-03-10 16:17:48 -0500

    some aa data

---
+++ data/aminoacids.js
@@ -0,0 +1,29 @@
+var aminoAcidArray = [{
+	environment: "pH ~ 7.4 physiological conditions",
+	name: "alanine",
+	tripleLetterSymbol: "ala",
+	singleLetterSymbol: "A",
+	molecularWeight: 89.10,
+	molecularFormula: C3H7NO2, 
+	essential: "non-essential",
+	hydrophobicity: "hyrophobic",
+	pKa: 2.34,
+	pKb: 9.69,
+	pI: 6.00,
+	skeletalImage: "alanineskeleton.png",
+	ballStickImage: "alaninestick.png"
+}, {
+	environment: "pH ~ 7.4 physiological conditions",
+	name: "arginine",
+	tripleLetterSymbol: "arg",
+	singleLetterSymbol: "R",
+	molecularWeight: 174.20,
+	molecularFormula: C6H14N4O2, 
+	essential: "non-essential",
+	hydrophobicity: "hyrophilic",
+	pKa: 2.17,
+	pKb: 9.04,
+	pI: 10.76,
+	skeletalImage: "arginineskeleton.png",
+	ballStickImage: "argininestick.png"
+}];
